4f516b107f1c10605b38d54f57b954434f1f11d3
Kamal Shrestha <kshrestha1@godaddy.com>
2025-04-26 18:49:14 +0545

    init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@repo/ui/button";
 export default function Page() {
   return (
     <div>
-      <h1>Hello World from web</h1>
+      <h1>Hello World from web  </h1>
       <Button onClick={() => alert("Button clicked")}>Click me</Button>
     </div>
   );
