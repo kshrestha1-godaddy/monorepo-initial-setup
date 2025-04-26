3f35bd1803ca6b2509b445e01cc9335a02be82d1
Kamal Shrestha <kshrestha1@godaddy.com>
2025-04-26 19:12:36 +0545

    init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import { Button } from "@repo/ui/button";
+import { Users } from "./components/Users";
+
 
 export default function Page() {
   return (
     <div>
       <h1>Hello World from web  </h1>
       <Button onClick={() => alert("Button clicked")}>Click me</Button>
+
+      <Users />
+
+  
     </div>
   );
 }
